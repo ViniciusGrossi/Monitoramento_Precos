cebfe06e47d51ad674392926775e0b519a559007
Vinicius Grossi <viniciussggrossi@gmail.com>
2024-06-02 19:45:49 -0300

    Correção do caminho da pasta

--- dashboards/app.py
+++ dashboards/app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import sqlite3
 
 # Conectar ao banco de dados SQLite
-conn = sqlite3.connect('../data/quotes.db')
+conn = sqlite3.connect('data/quotes.db')
 
 # Carregar os dados da tabela 'mercadolivre_items' em um DataFrame pandas
 df = pd.read_sql_query("SELECT * FROM mercadolivre_items", conn)
