ab66026c1af4a204abf6b25c3745c1f51e9121f6
Vinicius Grossi <viniciussggrossi@gmail.com>
2024-06-02 15:23:09 -0300

    Novas análises e gráficos adicionados

--- dashboards/app.py
+++ dashboards/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import sqlite3
 
 # Conectar ao banco de dados SQLite
@@ -29,24 +31,75 @@ col2.metric(label="Número de Marcas Únicas", value=unique_brands)
 average_new_price = df['new_price'].mean()
 col3.metric(label="Preço Médio Novo (R$)", value=f"{average_new_price:.2f}")
 
-# Quais marcas são mais encontradas até a 10ª página
-st.subheader('Marcas mais encontradas até a 10ª página')
-col1, col2 = st.columns([4, 2])
-top_10_pages_brands = df.head(500)['brand'].value_counts().sort_values(ascending=False)
-col1.bar_chart(top_10_pages_brands)
-col2.write(top_10_pages_brands)
+tab1, tab2, tab3 = st.tabs(['Preços', 'Marcas', 'Avaliações'])
+with tab1:
+    # Qual o preço médio por marca
+    st.subheader('Preço médio por marca')
+    col1, col2 = st.columns([4, 2])
+    average_price_by_brand = df.groupby('brand')['new_price'].mean().sort_values(ascending=False)
+    col1.bar_chart(average_price_by_brand)
+    col2.write(average_price_by_brand)
 
-# Qual o preço médio por marca
-st.subheader('Preço médio por marca')
-col1, col2 = st.columns([4, 2])
-average_price_by_brand = df.groupby('brand')['new_price'].mean().sort_values(ascending=False)
-col1.bar_chart(average_price_by_brand)
-col2.write(average_price_by_brand)
+    # Análise de Distribuição de Preços
+    col1, col2 = st.columns([4, 2])
+    st.subheader('Distribuição de Preços')
+    fig = px.histogram(df['new_price'].dropna(), nbins=30, labels={'value':'Preço (R$)'}, title='Histograma de Preços')
+    col1.plotly_chart(fig)
 
-# Qual a satisfação por marca
-st.subheader('Satisfação por marca')
-col1, col2 = st.columns([4, 2])
-df_non_zero_reviews = df[df['reviews_rating_number'] > 0]
-satisfaction_by_brand = df_non_zero_reviews.groupby('brand')['reviews_rating_number'].mean().sort_values(ascending=False)
-col1.bar_chart(satisfaction_by_brand)
-col2.write(satisfaction_by_brand)
+    st.subheader('Boxplot de Preços')
+    col1, col2 = st.columns([8, 2])
+    fig = px.box(df['new_price'].dropna(), labels={'value':'Preço (R$)'}, title='Boxplot de Preços')
+    st.plotly_chart(fig)
+    st.write(df['new_price'].describe())
+
+    # Correlação entre Preço e Avaliações
+    st.subheader('Correlação entre Preço e Avaliações')
+    correlation = df[['new_price', 'reviews_rating_number']].corr().iloc[0, 1]
+    st.write(f"A correlação entre preço e nota das avaliações é: {correlation:.2f}")
+    fig = px.scatter(df, x='new_price', y='reviews_rating_number', labels={'new_price':'Preço (R$)', 'reviews_rating_number':'Nota das Avaliações'})
+    st.plotly_chart(fig)
+
+    # Identificação de Outliers
+    st.subheader('Identificação de Outliers')
+    col1, col2 = st.columns([4, 2])
+    fig = px.box(df, x='new_price', points='all', labels={'x':'Preço (R$)'}, title='Outliers no Preço')
+    col1.plotly_chart(fig)
+    col2.write(df[df['new_price'] > df['new_price'].quantile(0.99)][['brand', 'new_price']])
+
+with tab2:
+    st.subheader('Preço médio por marca')
+    col1, col2 = st.columns([4, 2])
+    average_price_by_brand = df.groupby('brand')['new_price'].mean().sort_values(ascending=False)
+    col1.bar_chart(average_price_by_brand)
+    col2.write(average_price_by_brand)
+
+    # Quais marcas são mais encontradas até a 10ª página
+    st.subheader('Marcas mais encontradas até a 10ª página')
+    col1, col2 = st.columns([4, 2])
+    top_10_pages_brands = df.head(500)['brand'].value_counts().sort_values(ascending=False)
+    col1.bar_chart(top_10_pages_brands)
+    col2.write(top_10_pages_brands)
+
+    # Qual a satisfação por marca
+    st.subheader('Satisfação por marca')
+    col1, col2 = st.columns([4, 2])
+    df_non_zero_reviews = df[df['reviews_rating_number'] > 0]
+    satisfaction_by_brand = df_non_zero_reviews.groupby('brand')['reviews_rating_number'].mean().sort_values(ascending=False)
+    col1.bar_chart(satisfaction_by_brand)
+    col2.write(satisfaction_by_brand)
+    
+with tab3:
+    # Análise de Avaliações
+    st.subheader('Distribuição de Avaliações')
+
+    fig = px.histogram(df['reviews_rating_number'].dropna(), nbins=30, labels={'value':'Número de Avaliações'}, title='Histograma de Avaliações')
+    st.plotly_chart(fig)
+
+    df_non_zero_reviews = df[df['reviews_rating_number'] > 0]
+    fig = px.box(df_non_zero_reviews['reviews_rating_number'].dropna(), labels={'value':'Número de Avaliações'}, title='Boxplot de Avaliações')
+    st.plotly_chart(fig)
+    st.write(df['reviews_rating_number'].describe())
+
+    # Identificação de Outliers
+    fig = px.box(df, x='reviews_rating_number', points='all', labels={'x':'Número de Avaliações'}, title='Outliers nas Avaliações')
+    st.plotly_chart(fig)
